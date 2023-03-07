47b19ca9325db340ac5e395101cfce16a12b2647
LIDIA GABRIELLY DO NASCIMENTO GALDINO <22102372@CFP127ALUNO.LOCAL>
2023-03-07 10:53:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Login from './views/login/Login'
+
+function App() {
+
+  return (
+    <Login />
+  )
+}
+
+export default App
---
+++ src/views/login/Login.tsx
@@ -0,0 +1,96 @@
+import { motion } from 'framer-motion'
+import React, { useState } from 'react'
+import './circle.css'
+
+const Login = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [isSignInOpen, setSignIn] = useState(true)
+  const [isSignInContainerOpen, setSignInContaienr] = useState(true)
+  const [isSignUpOpen, setSignUp] = useState(false)
+  const [isSignUpContainerOpen, setSignUpContaienr] = useState(false)
+
+
+  const circleVariants = {
+    hidden: {
+        scale: 1
+    },
+    visible: {
+        scale: 180,
+        transition: {
+            type: 'spring',
+            stiffness: 160,
+            damping: 60
+        }
+    }
+}
+
+const signInVariants = {
+  hidden: {
+      opacity: 0
+  },
+  visible: {
+      opacity: 1,
+      transition: {
+          delay: 0.5
+      }
+  }
+}
+const signUpVariants = {
+  hidden: {
+      opacity: 0
+  },
+  visible: {
+      opacity: 1,
+      transition: {
+          delay: 0.5
+      }
+  }
+}
+
+
+const signInContainerVariants = {
+  hidden: {
+      x: 1,
+      y: -0.50,
+      right: 0.52
+  },
+  visible: {
+    y: -50,
+      transition: {
+          delay: 0.2
+      }
+  }
+}
+
+const signUpContainerVariants = {
+  hidden: {
+    translateX: -100
+},
+visible: {
+  translateX: 100,
+    transition: {
+        delay: 0.2
+    }
+}
+}
+  
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)}>Registre-se</button>
+      <motion.div variants={signInContainerVariants} initial='visible' animate={isSignInContainerOpen ? 'visible' : 'hidden'}>
+        
+      </motion.div>
+      <motion.div variants={signUpContainerVariants} initial='hidden' animate={isSignUpContainerOpen ? 'visible' : 'hidden'}></motion.div>
+
+      <motion.div className='bola' variants={circleVariants} initial='hidden' animate={isOpen ? 'visible' : 'hidden'} ></motion.div>
+
+      <motion.div variants={signInVariants} initial='visible' animate={isSignInOpen ? 'visible' : 'hidden'}>
+
+      </motion.div>
+      <motion.div variants={signUpVariants} initial='hidden' animate={isSignUpOpen ? 'visible' : 'hidden'}></motion.div>
+    </div>
+    
+  )
+}
+
+export default Login
