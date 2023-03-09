5f86d6159add5fc7e8f99c9734bc6e0e66bffc97
lidiagaldino <lidiagabrielly14@gmail.com>
2023-03-09 11:30:11 -0300

    incio integração, animações e formularios

--- src/views/login/components/Form.tsx
+++ src/views/login/components/Form.tsx
@@ -1,26 +1,57 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import logo from '../../../assets/logo.png'
-import formm from '../../../views/login/form.css'
-import '../../../views/login/switchButton.css'
+import '../styles/formSignIn.css'
+import '../styles/button.css'
 import { Person, Envelope } from 'phosphor-react'
-import * as Switch from '@radix-ui/react-switch';
+import { api } from '../../../lib/axios'
 
 const form = () => {
+  const [emailState, setEmailState] = useState({ value: '' })
+  const [passState, setPassState] = useState({ value: '' })
+
+
+
+  function handleEmailChange(event: ChangeEvent<HTMLInputElement>): void {
+    setEmailState({ value: event.target.value })
+  }
+
+  function handlePassChange(event: ChangeEvent<HTMLInputElement>): void {
+    setPassState({ value: event.target.value })
+  }
+
+  async function login(event: FormEvent) {
+    event.preventDefault()
+
+    if (!emailState.value || !passState.value) {
+      return
+    }
+
+    const teste = await api.post('/user/auth', {
+      email: emailState,
+      senha: passState
+    })
+
+    setEmailState({value: ''})
+    setPassState({value: ''})
+
+    console.log(teste)
+  }
+
   return (
-    <form action="#" className="sign-in-form" id="sign-in-form">
+    <form onSubmit={login} className="sign-in-form">
       <img src={logo} className='logoo' alt="logo" />
       <h2 className='title'>Entrar</h2>
       <div className="input-field">
         <i><Person /></i>
-        <input type="email" placeholder="Email" id="email-login" required />
+        <input autoComplete='current-password' type="email" name='email' placeholder="Email" value={emailState.value} onChange={handleEmailChange} />
       </div>
 
       <div className="input-field">
         <i><Envelope /></i>
-        <input type="password" placeholder="Senha" id="senha-login" required />
+        <input value={passState.value} name='pass' type="password" placeholder="Senha" onChange={handlePassChange} />
       </div>
 
-      <button type="submit" value="Entrar" className="btn solid" id="sign-in">Entrar</button>
+      <button type="submit" value="Entrar" className="btn solid">Entrar</button>
 
 
     </form>
